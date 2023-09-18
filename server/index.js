279115d931b3e7604172d2e857c5e5e3515e57a0
Moyen Uddin <115378456+moyenu2000@users.noreply.github.com>
2023-09-18 23:29:02 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors(
 ));
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://moyenu2000:BzPA87Us0AkH5jTw@cluster0.qpsygng.mongodb.net/?retryWrites=true&w=majority');
 
 
 app.get("/", (req, res) => {
